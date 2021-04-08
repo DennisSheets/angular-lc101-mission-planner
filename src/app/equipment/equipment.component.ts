f0d72f63a647e0d991da8101bad51b59e256da7d
madsur <dennissheets@outlook.com>
2021-04-07 22:45:35 -0500

    completed 30.7.2.1, started 7.2.2

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -21,10 +21,21 @@ export class EquipmentComponent implements OnInit {
    cargoMass: number = 0;
    maximumAllowedMass: number = 2000;
    maxItems: number = 10;
+  
 
    constructor() { }
 
    ngOnInit() { }
+
+   addItem(item){
+    this.cargoHold.push(item);
+    this.cargoMass = this.cargoMass + item.mass;
+    if(this.maximumAllowedMass - this.cargoMass <=200){
+      return true;
+    } else {
+      return false
+    }
+   }
 
    // Code your addItem function here:
    
